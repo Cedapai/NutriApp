a6cf8fb86e812dd9daada636167deb630b206e7c
Cesar Betancourt <cesarebetan@gmail.com>
2018-11-20 18:07:23 -0600

    Add show registers

--- app/src/main/java/com/example/betancourt/nutriapp/LandingUserActivity.java
+++ app/src/main/java/com/example/betancourt/nutriapp/LandingUserActivity.java
@@ -5,23 +5,46 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import com.example.betancourt.nutriapp.adapters.FoodListAdapter;
+import com.example.betancourt.nutriapp.adapters.RegisterListAdapter;
+import com.example.betancourt.nutriapp.pojo.Food;
+import com.example.betancourt.nutriapp.pojo.Register;
 import com.example.betancourt.nutriapp.pojo.User;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LandingUserActivity extends AppCompatActivity {
 
     private TextView txtEmailUser;
     private FirebaseAuth firebaseAuth;
     FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private static final String TAG = "Firelog";
+    private RecyclerView mRegisterlList;
+    private FirebaseFirestore mFirestore;
+    private RegisterListAdapter registerListAdapter;
+    private List<Register> registerList;
+
+    public FirebaseStorage mStorage;
+    public StorageReference mStorageRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +76,39 @@ public class LandingUserActivity extends AppCompatActivity {
                     }
                 } else {
                     Log.d("Firestore", "get failed with ", task.getException());
+                }
+            }
+        });
+
+        // Adapter
+        registerList = new ArrayList<>();
+        registerListAdapter = new RegisterListAdapter(getApplicationContext(), registerList);
+
+        mRegisterlList = (RecyclerView) findViewById(R.id.register_list);
+        mRegisterlList.setHasFixedSize(true);
+        mRegisterlList.setLayoutManager(new LinearLayoutManager(this));
+        mRegisterlList.setAdapter(registerListAdapter);
+
+        mFirestore = FirebaseFirestore.getInstance();
+
+        mFirestore.collection("users").document(userUid).collection("registers").orderBy("hour").addSnapshotListener(new EventListener<QuerySnapshot>() {
+            @Override
+            public void onEvent(QuerySnapshot documentSnapshots, FirebaseFirestoreException e) {
+                if(e != null){
+                    Log.d(TAG, "Error: " + e.getMessage());
+                }
+
+                for(DocumentChange doc: documentSnapshots.getDocumentChanges()){
+                    if(doc.getType() == DocumentChange.Type.ADDED){
+
+                        String register_id = doc.getDocument().getId();
+                        Log.e(TAG, register_id);
+                        Register register = doc.getDocument().toObject(Register.class).withId(register_id);
+                        registerList.add(register);
+                        Log.e(TAG, register.toString());
+
+                        registerListAdapter.notifyDataSetChanged();
+                    }
                 }
             }
         });
--- app/src/main/java/com/example/betancourt/nutriapp/adapters/RegisterListAdapter.java
+++ app/src/main/java/com/example/betancourt/nutriapp/adapters/RegisterListAdapter.java
@@ -1,4 +1,88 @@
 package com.example.betancourt.nutriapp.adapters;
 
-public class RegisterListAdapter  {
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.betancourt.nutriapp.R;
+import com.example.betancourt.nutriapp.pojo.Register;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.List;
+
+public class RegisterListAdapter extends RecyclerView.Adapter<RegisterListAdapter.ViewHolder> {
+
+    private static final String TAG = "Firelog";
+    public List<Register> registerList;
+    public Context context;
+    public FirebaseStorage storage;
+    public StorageReference storageRef;
+
+    public RegisterListAdapter(Context context, List<Register> registerList){
+        this.registerList = registerList;
+        this.context = context;
+        storage = FirebaseStorage.getInstance();
+
+        //mFirestore = FirebaseFirestore.getInstance();
+    }
+
+    @Override
+    public RegisterListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.layout_register, parent, false);
+        return new RegisterListAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final RegisterListAdapter.ViewHolder holder, int position) {
+        holder.nombreRegister.setText(registerList.get(position).getFood_name());
+        holder.typeRegister.setText(registerList.get(position).getFood_type());
+        holder.cantidadRegister.setText(registerList.get(position).getQuantity());
+        holder.fechaHoraRegister.setText((registerList.get(position).getDate() + " - " + registerList.get(position).getHour()));
+        holder.marcaRegister.setText(registerList.get(position).getBrand());
+        holder.quienRegister.setText(registerList.get(position).getWho());
+        holder.actividadRegister.setText(registerList.get(position).getActivity());
+
+        final String register_id = registerList.get(position).objectId;
+
+        /*holder.mView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(context,FoodActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(intent);
+            }
+        });*/
+    }
+
+    @Override
+    public int getItemCount() {
+        return registerList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        View mView;
+
+        public TextView nombreRegister, typeRegister, cantidadRegister, fechaHoraRegister, marcaRegister, quienRegister, actividadRegister;
+        public ImageView foodImage;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            mView = itemView;
+
+            nombreRegister = (TextView) mView.findViewById(R.id.nombre_register);
+            typeRegister = (TextView) mView.findViewById(R.id.type_register);
+            cantidadRegister = (TextView) mView.findViewById(R.id.cantidad_register);
+            fechaHoraRegister = (TextView) mView.findViewById(R.id.fecha_hora_register);
+            marcaRegister = (TextView) mView.findViewById(R.id.marca_register);
+            quienRegister = (TextView) mView.findViewById(R.id.quien_resgister);
+            actividadRegister = (TextView) mView.findViewById(R.id.actividad_resgister);
+        }
+    }
+
 }
